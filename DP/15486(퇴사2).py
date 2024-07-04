cb61a6d11421342f98de39df429f064d7bb1cef5
Solxcero <rumbini98@gmail.com>
2024-07-04 16:34:36 +0900

    25일

---
+++ DP/15486(퇴사2).py
@@ -0,0 +1,63 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0]*(N+1)
+max_income = 0 
+days = []
+
+for i in range(N):
+    t, p = map(int,input().split())
+    if i + t > N:
+        continue
+    days.append((i+1, t, p))
+print(days)
+
+
+
+
+
+'''
+10
+1 1
+1 2
+1 3
+1 4
+1 5
+1 6
+1 7
+1 8
+1 9
+1 10
+
+> 55
+
+10
+5 10
+5 9
+5 8
+5 7
+5 6
+5 10
+5 9
+5 8
+5 7
+5 6
+
+> 20
+
+10
+5 50
+4 40
+3 30
+2 20
+1 10
+1 10
+2 20
+3 30
+4 40
+5 50
+
+> 90
+'''
