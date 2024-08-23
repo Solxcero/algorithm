188df7e2a82a4e53cd79a598847285ca7af904ec
Solxcero <rumbini98@gmail.com>
2024-08-23 17:11:16 +0900

    이진끝

---
+++ Binary Search/10815(숫자카드).py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())
+arrN = list(map(int,input().split()))
+M = int(input())
+arrM = list(map(int,input().split()))
+
+arrN.sort()
+
+def find_num(target):
+    left, right = 0, N-1
+    while left <= right :
+        mid = (left + right)//2
+        
+        if arrN[mid] == target:
+            return 1
+        
+        elif arrN[mid] > target:
+            right = mid - 1
+        
+        else:
+            left = mid + 1
+    return 0
+            
+for target in arrM:
+    print(find_num(target),end=' ')
