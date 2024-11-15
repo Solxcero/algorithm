06b9d177743c775db0efd4a4f2a108ca4df552c8
Solxcero <rumbini98@gmail.com>
2024-11-15 12:30:25 +0900

    LinkedList

--- Basic/LinkedList.py
+++ Basic/LinkedList.py
@@ -38,7 +38,7 @@ class Solution:
         return dummy_head.next
     
 # Solution 2 : time, memory better
-class Solution:
+class Solution2:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy_head = ListNode(0)
         current = dummy_head
@@ -61,3 +61,35 @@ class Solution:
 
         # 결과 연결 리스트의 시작점 반환
         return dummy_head.next
+    
+# 리스트를 ListNode로 변환하는 함수
+def list_to_listnode(numbers):
+    dummy_head = ListNode(0)
+    current = dummy_head
+    for num in numbers:
+        current.next = ListNode(num)
+        current = current.next
+    return dummy_head.next
+
+# ListNode를 리스트로 변환하는 함수
+def listnode_to_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+# 입력 리스트를 ListNode 형식으로 변환
+l1 = list_to_listnode([4, 3, 2])  # 234
+l2 = list_to_listnode([1, 2])     # 21
+
+l1_v = ListNode(4, ListNode(3, ListNode(2)))
+l2_v = ListNode(1, ListNode(2))
+# Solution2 실행
+sol = Solution2()
+result = sol.addTwoNumbers(l1, l2)
+result2 = sol.addTwoNumbers(l1_v, l2_v)
+
+# 결과를 다시 리스트로 변환하여 출력
+print(listnode_to_list(result))  # 출력: [5, 5, 2] (결과: 255)
+print(listnode_to_list(result2))  # 출력: [5, 5, 2] (결과: 255)
